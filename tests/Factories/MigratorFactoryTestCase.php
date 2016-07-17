a979416673c154774ab905bcfc03482d75e119fc
ggordon <ggg_gilroy@yahoo.com>
2016-07-17 20:22:20 -0300

    Added ./tests/Factories/MigratorFactoryTestCase.php

---
+++ tests/Factories/MigratorFactoryTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use IGonics\Migrations\Laravel\5\1\SpecificFilesMigrator as L51SpecificFilesMigrator;
+use IGonics\Migrations\Laravel\5\2\SpecificFilesMigrator as L52SpecificFilesMigrator;
+use IGonics\Migrations\Factories\MigratorFactory;
+
+class MigratorFactoryTestCase extends PHPUnit_Framework_TestCase {
+
+}
